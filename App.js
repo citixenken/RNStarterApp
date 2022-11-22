efa0000e1bde1330e5f1b21dd8f7db2df6b86d31
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-11-23 00:15:36 +0300

    added custom fonts

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import { useFonts } from "expo-font";
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 
-const Stack = createStackNavigator();
-
 const theme = {
   ...DefaultTheme,
   colors: {
@@ -15,8 +13,19 @@ const theme = {
   },
 };
 
+const Stack = createStackNavigator();
+
 const App = () => {
-  const [loaded] = useFonts({});
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
