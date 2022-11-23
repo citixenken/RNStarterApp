a1957ef1b76de1f12dc9784027b342f2a09a07b0
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-11-23 16:35:53 +0300

    feature: Card component

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,7 @@
 import { TouchableOpacity, View, Text, Image } from "react-native";
 import React from "react";
 import { COLORS, SIZES, FONTS, SHADOWS } from "../constants";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   return (
@@ -30,10 +31,28 @@ export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   );
 };
 
-export const RectButton = () => {
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
   return (
-    <View>
-      <Text>Rectangle Button</Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        padding: SIZES.small,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: fontSize,
+          color: COLORS.white,
+          textAlign: "center",
+        }}
+      >
+        Place a bid
+      </Text>
+    </TouchableOpacity>
   );
 };
--- components/NFTCard.js
+++ components/NFTCard.js
@@ -3,6 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 import { CircleButton, RectButton } from "./Button";
+import { NFTTitle, EthPrice, SubInfo } from "./SubInfo";
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -31,7 +32,30 @@ const NFTCard = ({ data }) => {
 
         <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>NFT Card</Text>
+      <SubInfo />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: SIZES.font,
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
